e310e26cd9924afa6c97c72004b4bb95d96c3ff9
uiqkos <57287053+uiqkos@users.noreply.github.com>
2022-04-02 13:12:42 +0300

    вынесены комментарии, рефакторинг

--- lib/pages/nya_results_page.dart
+++ lib/pages/nya_results_page.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:nya_mobile/data/nya_caching.dart';
 import 'package:nya_mobile/data/nya_request_model.dart';
 import 'package:nya_mobile/widgets/comments/nya_comment_widget.dart';
 import 'package:provider/provider.dart';
 
 import '../data/nya_comment.dart';
-import '../data/nya_predict_response.dart';
 
 class NyaResultsPage extends StatefulWidget {
-  final List<NyaComment> comments = [];
 
   NyaResultsPage({Key? key}) : super(key: key);
 
@@ -16,98 +15,89 @@ class NyaResultsPage extends StatefulWidget {
 }
 
 class _NyaResultsPageState extends State<NyaResultsPage> {
+
   @override
   Widget build(BuildContext context) {
     var requestModel = context.watch<NyaPredictRequestModel>();
 
-    late Widget content;
-
-    if (requestModel.isEmpty) {
-      content = Padding(
-        padding: const EdgeInsets.fromLTRB(20, 150, 20, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Результатов пока нет', style: Theme
-                .of(context)
-                .textTheme
-                .headline4),
-            const Image(image: AssetImage('assets/images/idk.png')),
-          ],
-        )
-      );
-    } else {
-      content = FutureBuilder<NyaPredictResponse>(
-        future: requestModel.get(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            print(snapshot.stackTrace);
-            print(snapshot.error);
-          }
-          else if (snapshot.hasData) {
-            var response = snapshot.data!;
-            var insertIndex = 0;
-            var path = List.from(response.path);
-
-            while (path.isNotEmpty) {
-              var parentId = path.removeAt(0);
-
-              while (
-                insertIndex < widget.comments.length
-              ) {
-
-                if (widget.comments.elementAt(insertIndex).id != parentId) {
-                  insertIndex += 1;
-                  break;
-                }
-                insertIndex += 1;
-              }
-
-            }
-
-            widget.comments.insertAll(insertIndex, response.items);
-          }
-
-          var children = <Padding>[];
-
-          for (var comment in widget.comments) {
-            children.add(
-              Padding(
-                padding: EdgeInsets.only(left: 30.0 * comment.level),
+    return FutureBuilder<List<NyaComment>?>(
+        future: NyaCacherProvider.provide('request').getCache(
+            requestModel.request.toString(),
+            requestModel.getComments
+        ),
+        builder: (
+            BuildContext context,
+            AsyncSnapshot<List<NyaComment>?> snapshot
+        ) {
+          idk(String message) => Padding(
+                padding: const EdgeInsets.fromLTRB(20, 150, 20, 0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    NyaCommentWidget(comment: comment),
-                    SizedBox(
-                      width: 120,
-                      height: 30,
-                      child: TextButton(
-                        child: const Text('Провести анализ', style: TextStyle(
-                          fontSize: 10,
-                        ),),
-                        onPressed: () {
-                          print(comment.path.join('/'));
-                          requestModel.updateExpandPath(comment.path.join('/'));
-                        }
-                      ),
-                    ),
+                    Text(message,
+                        style: Theme
+                            .of(context)
+                            .textTheme
+                            .headline4),
+                    const Image(image: AssetImage('assets/images/idk.png')),
                   ],
                 )
-              )
             );
+
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return idk('Результатов пока нет');
+
+            case ConnectionState.waiting:
+              return const Image(image: AssetImage('assets/images/catloading.gif'));
+
+            default:
+              var comments = <NyaComment>[];
+              if (snapshot.hasError) {
+                print(snapshot.stackTrace.toString());
+                return idk(snapshot.error.toString());
+              } else if (snapshot.hasData) {
+                comments = snapshot.data!;
+              } else {
+                return idk('Результатов пока нет');
+              }
+
+              var children = <Padding>[];
+
+              for (var comment in comments) {
+                children.add(Padding(
+                    padding: EdgeInsets.only(left: 30.0 * comment.level),
+                    child: Column(
+                      children: [
+                        NyaCommentWidget(comment: comment),
+                        if (comment.comments > 0)
+                          SizedBox(
+                            width: 120,
+                            height: 30,
+                            child: TextButton(
+                                child: const Text(
+                                    'показать комментарии',
+                                    style: TextStyle(fontSize: 10)
+                                ),
+                                onPressed: () {
+                                  var path = comment.path.join('/');
+                                  requestModel.nextPage(path);
+                                }),
+                          )
+                        ]
+                      ),
+                    ));
+              }
+
+              return Scaffold(
+                  body: SingleChildScrollView(
+                      child: Column(
+                          children: children
+                      )
+                  )
+              );
           }
-
-          return Scaffold(
-            body: SingleChildScrollView(
-              child: Column(
-                children: children
-              )
-            )
-          );
-        }
-      );
-    }
-
-    return content;
+        });
   }
 }
